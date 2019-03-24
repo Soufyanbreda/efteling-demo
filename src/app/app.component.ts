9907c536fd49781884552c7bcb391cd79622e5c5
Soufyanbreda <soufyanb2@hotmail.com>
2019-03-24 19:51:11 +0100

    Add correct path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'efteling-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: '.efteling-demo/app.component.html',
+  styleUrls: ['.efteling-demo/app.component.scss']
 })
 export class AppComponent {
   title = 'efteling-web';
